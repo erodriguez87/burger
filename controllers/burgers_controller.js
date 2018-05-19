d661009225c0654253c4241b4c8716877e36af3f
Rodriguez <Eddie.Rodriguez@cpk.jmhuber.com>
2018-05-18 21:27:05 -0400

    updating the controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,19 +8,19 @@
 // Create all our routes and set up logic within those routes where required.
   router.get("/", function(req, res) {
     burger.selectAall(function(data) {
-      var burgie = {
-        burger: data
+      var hbsObj = {
+        burgers: data
       };
-      console.log(burgie);
-      res.render("index", burgie);
+      console.log(hbsObj);
+      res.render("index", hbsObj);
     });
   });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "sleepy"
+  burger.insertOne([
+     'burger_name', 'devoured'
   ], [
-    req.body.name, req.body.sleepy
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -36,19 +36,6 @@ router.put("/api/burgers/:id", function(req, res) {
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
